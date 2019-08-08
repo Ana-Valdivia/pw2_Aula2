1b86b0c92e4b3c3f7ca0b9f267d30f02a30e3ead
Victor da Costa <vicc1999@outlook.com>
2019-08-07 22:00:24 -0300

    Exercicio Media

---
+++ dist/exercicio.js
@@ -0,0 +1,9 @@
+var n1 = 10;
+var n2 = 9;
+var n3 = 5;
+var media = (n1 + n2 + n3) / 3;
+console.log("Sua média  é " + media);
+if (media >= 7)
+    console.log("Voce esta aprovado");
+else
+    (console.log("Voce esta REPROVADO"));
---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+let n1 : number = 10
+let n2 : number = 9
+let n3 : number = 5
+let media = (n1 + n2 + n3)/3
+console.log("Sua média  é " + media)
+if  (media >=7)console.log("Voce esta aprovado")
+
+else (console.log("Voce esta REPROVADO"))
+
+   
+
